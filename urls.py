6f45edbe88fe4325048b9662703f49951f7c91bd
Keyan Pishdadian <kpishdadian@gmail.com>
2020-03-01 18:47:33 -0800

    Add union map

--- urls.py
+++ urls.py
@@ -4,6 +4,14 @@ addrs = {
     'lincoln_park': "-123.109495,45.527719",
     'talisman_city_park': "-123.126243,45.528791",
     'thatcher_city_park': "-123.134358,45.535558",
+    'joseph_gale': "-123.093986,45.516187",
+    'bard_park': "-123.095529,45.522748",
+    'Rogers_park': "-123.103958,45.516644",
+    'Harlemen_park': "-123.062675,45.513251",
+    'Tarrybrooke_park': "-123.067173,45.515927",
+    'stites_city_park': "-123.117109,45.527630",
+    'hazel_sills_park': "-123.119122,45.531580",
+    'roth_family_park': "-123.140526,45.527797",
 }
 
 with open('fetch.sh', 'w') as f:
